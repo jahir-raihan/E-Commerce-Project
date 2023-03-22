47799acac3e2b3acd4c6e4330a0ad989e47ce228
jahir-raihan <raihandevbd@gmail.com>
2023-03-22 20:24:06 +0600

    Completed making Home page Responsive

--- Site Templates/js/index.js
+++ Site Templates/js/index.js
@@ -34,3 +34,46 @@ listItems.forEach(function(item) {
 
 
 // End Search suggestion and selection
+
+
+// Toggle Nav menu
+
+function toggle_nav_menu(){
+    var obj = document.getElementById('nav-ham-menu');
+    var obj2 = document.getElementById('cross-icon-nav-ham')
+    var toggle_nav = document.getElementById('toggle-nav')
+    if (obj.contains(obj2)){
+        obj.innerHTML = '<img src="/img/menu.png" alt="">'
+        toggle_nav.style.display = 'none'
+    }else{
+        obj.innerHTML = '<i id="cross-icon-nav-ham" class="fa fa-times"></i>'
+        toggle_nav.style.display = 'block'
+
+
+    }
+}
+
+// End Toggle Nav menu
+
+// To change background color of hero
+
+
+
+ // array of background colors to cycle through
+ const bgColors = ['pink', '#E5E0FF', 'pink', '#F8CBA6', '#AACB73'];
+
+ // get reference to the body element
+ const body = document.getElementById('hero');
+
+ // initialize color index
+ let colorIndex = 0;
+
+ // set interval to change background color every 1 minute
+ setInterval(() => {
+   // change background color to next color in array
+   body.style.backgroundColor = bgColors[colorIndex];
+
+   // increment color index, reset to 0 if end of array is reached
+   colorIndex = (colorIndex + 1) % bgColors.length;
+ }, 5000);
+// End change color hero
