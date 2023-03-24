a740664f3594a726f0f172961ce08538bc095d72
jahir-raihan <raihandevbd@gmail.com>
2023-03-24 17:24:29 +0600

    Added price range functionality for all devices

--- Site Templates/js/products.js
+++ Site Templates/js/products.js
@@ -1,46 +1,77 @@
 // Price Range 
 
 let rangeMin = 100;
-const range = document.querySelector(".range-selected");
+const range = document.querySelectorAll(".range-selected");
 const rangeInput = document.querySelectorAll(".range-input input");
 const rangePrice = document.querySelectorAll(".range-price input");
 
 
 rangeInput.forEach((input) => {
-    input.addEventListener("input", (e) => {
-      let minRange = parseInt(rangeInput[0].value);
-      let maxRange = parseInt(rangeInput[1].value);
-      if (maxRange - minRange < rangeMin) {     
-        if (e.target.className === "min") {
-          rangeInput[0].value = maxRange - rangeMin;        
-        } else {
-          rangeInput[1].value = minRange + rangeMin;        
-        }
+  input.addEventListener("input", (e) => {
+    let minRange = parseInt(rangeInput[2].value);
+    let maxRange = parseInt(rangeInput[3].value);
+    if (maxRange - minRange < rangeMin) {     
+      if (e.target.className === "min") {
+        rangeInput[2].value = maxRange - rangeMin;        
       } else {
-        rangePrice[0].value = minRange;
-        rangePrice[1].value = maxRange;
-        range.style.left = (minRange / rangeInput[0].max) * 100 + "%";
-        range.style.right = 100 - (maxRange / rangeInput[1].max) * 100 + "%";
+        rangeInput[3].value = minRange + rangeMin;        
       }
-    });
+    } else {
+      rangePrice[2].value = minRange;
+      rangePrice[3].value = maxRange;
+      range[1].style.left = (minRange / rangeInput[2].max) * 100 + "%";
+      range[1].style.right = 100 - (maxRange / rangeInput[3].max) * 100 + "%";
+    }
+
+    // Mobile Version
+
+    let minRange1 = parseInt(rangeInput[0].value);
+    let maxRange1 = parseInt(rangeInput[1].value);
+    if (maxRange1 - minRange1 < rangeMin) {     
+      if (e.target.className === "min") {
+        rangeInput[0].value = maxRange1 - rangeMin1;        
+      } else {
+        rangeInput[1].value = minRange1 + rangeMin1;        
+      }
+    } else {
+      rangePrice[0].value = minRange1;
+      rangePrice[1].value = maxRange1;
+      range[0].style.left = (minRange1 / rangeInput[0].max) * 100 + "%";
+      range[0].style.right = 100 - (maxRange1 / rangeInput[1].max) * 100 + "%";
+    }
   });
+});
 
 
-  rangePrice.forEach((input) => {
-    input.addEventListener("input", (e) => {
-      let minPrice = rangePrice[0].value;
-      let maxPrice = rangePrice[1].value;
-      if (maxPrice - minPrice >= rangeMin && maxPrice <= rangeInput[1].max) {
-        if (e.target.className === "min") {
-          rangeInput[0].value = minPrice;
-          range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
-        } else {
-          rangeInput[1].value = maxPrice;
-          range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-        }
+rangePrice.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = rangePrice[2].value;
+    let maxPrice = rangePrice[3].value;
+    if (maxPrice - minPrice >= rangeMin && maxPrice <= rangeInput[3].max) {
+      if (e.target.className === "min") {
+        rangeInput[2].value = minPrice;
+        range[1].style.left = (minPrice / rangeInput[2].max) * 100 + "%";
+      } else {
+        rangeInput[3].value = maxPrice;
+        range[1].style.right = 100 - (maxPrice / rangeInput[3].max) * 100 + "%";
       }
-    });
+    }
+    
+    // Mobile Version
+    
+    let minPrice1 = rangePrice[0].value;
+    let maxPrice1 = rangePrice[1].value;
+    if (maxPrice1 - minPrice1 >= rangeMin && maxPrice1 <= rangeInput[1].max) {
+      if (e.target.className === "min") {
+        rangeInput[0].value = minPrice1;
+        range[0].style.left = (minPrice1 / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxPrice1;
+        range[0].style.right = 100 - (maxPrice1 / rangeInput[1].max) * 100 + "%";
+      }
+    }
   });
+});
 
 // End Price Range
 
